dc88320ac4f85df09e00401ca832943f6ddb56b3
John Obi <johngorithm@gmail.com>
2018-10-04 19:15:35 +0100

    ch(testing): setup testing [Finishes #160808640]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './static/syles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Good Morning Dera</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
