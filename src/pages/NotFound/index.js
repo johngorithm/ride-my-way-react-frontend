4c299596a96974a8afae4699f0756d37e19f4307
John Obi <johngorithm@gmail.com>
2018-10-30 21:50:03 +0100

    deployment

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './notfound.css';
+
+const NotFound = () => {
+  return (
+    <div id="not-found">
+      <h1>404 <br/> Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/store/actions/rideActions.js
+++ src/store/actions/rideActions.js
@@ -8,6 +8,7 @@ import {
   CREATE_RIDE_STARTED,
   CREATE_RIDE_FAILED
 } from 'constants';
+import { closeModal } from 'actions/modalActions';
 import { ADD_AUTH_ERROR_MSG } from '../constants';
 
 export const getRides = () => async dispatch => {
@@ -37,7 +38,7 @@ export const createRide = (rideInfo)=> async dispatch => {
   })
   try {
     const res = await axios.post(`https://ride-m-way.herokuapp.com/api/v1/users/rides`, rideInfo, { headers: { 'x-access-token': JSON.parse(localStorage.getItem('token')) } });
-
+    dispatch(closeModal())
     return dispatch({
       type: CREATE_RIDE_SUCCESS,
       ride: res.data.ride,
